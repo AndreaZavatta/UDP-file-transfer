f97e3b50187ebac132fbc468aa6341b8f9a5f365
Luca Pasini <pasini.luca09@gmail.com>
2022-07-26 15:10:19 +0200

    Fix case in client and implement send_file

--- client.py
+++ client.py
@@ -1,19 +1,21 @@
 from socket import *
+from settings import *
 
 
 def send_message(msg):
-    clientSocket.sendto(msg.encode(), (serverName, serverPort))
+    client_socket.sendto(msg.encode(), (SERVER_NAME, SERVER_PORT))
 
 
-def send_file(file_name):
-    fi = open(file_name, 'rb')
-    clientSocket.sendto(fi, (serverName, serverPort))
-    fi.close()
+def send_file(file_path):
+    with open(file_path, 'rb') as file_io:
+        packages = file_io.read().__len__() // BUFFER_SIZE + 1
+        for i in range(packages):
+            msg = file_io.read(BUFFER_SIZE)
+            client_socket.sendto(msg, (SERVER_NAME, SERVER_PORT))
+        client_socket.sendto(b'EOF', (SERVER_NAME, SERVER_PORT))
 
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+client_socket = socket(AF_INET, SOCK_DGRAM)
 file_prefix = 'clientFiles/'
 while True:
     message = input('Input a command between list, get, put or quit to exit: ')
@@ -21,18 +23,21 @@ while True:
     match command:
         case 'list':
             send_message('list')
-            file_list = clientSocket.recv(1024)
+            file_list = client_socket.recv(BUFFER_SIZE)
             print(file_list.decode())
         case 'get':
             file_name = message.split(' ')[1]
             send_message(message)
-            file = clientSocket.recv(1024)
+            file = client_socket.recv(BUFFER_SIZE)
             f = open(file_prefix + file_name, 'wb')
             f.write(file.strip())
+            f.close()
         case 'put':
             file_name = message.split(' ')[1]
             send_message(message)
             send_file(file_prefix + file_name)
         case 'quit':
             break
-clientSocket.close()
+        case default:
+            print('Invalid command')
+client_socket.close()
