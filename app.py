36898a26037b679d6990f492900baeffbaaf8eab
Andrea Zavatta <zavattaandrea@gmail.com>
2022-07-26 17:35:54 +0200

    create upload bar and pass values from html to python functions

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+from urllib import request
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def init():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/upload-image/<url>', methods=['GET', 'POST'])
+def uploadFile(url):  # put application's code here
+    return url
 
 
 if __name__ == '__main__':
