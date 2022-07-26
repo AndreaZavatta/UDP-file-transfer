e489de704303adde398c3da059175d7aff59c610
Luca Pasini <pasini.luca09@gmail.com>
2022-07-26 12:11:33 +0200

    Fix split of message

--- client.py
+++ client.py
@@ -17,20 +17,22 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 file_prefix = 'clientFiles/'
 while True:
     message = input('Input a command between list, get, put or quit to exit: ')
-    command, filename = message.split(' ')
+    command = message.split(' ')[0]
     match command:
         case 'list':
             send_message('list')
             file_list = clientSocket.recv(1024)
             print(file_list.decode())
         case 'get':
+            file_name = message.split(' ')[1]
             send_message(message)
             file = clientSocket.recv(1024)
-            f = open(file_prefix + filename, 'wb')
+            f = open(file_prefix + file_name, 'wb')
             f.write(file.strip())
         case 'put':
+            file_name = message.split(' ')[1]
             send_message(message)
-            send_file(file_prefix + filename)
+            send_file(file_prefix + file_name)
         case 'quit':
             break
 clientSocket.close()
