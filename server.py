cf73ec803b59748d03b2f6e7142eb6be7599c767
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-07-26 15:36:28 +0200

    Fix naming conventions and implement receive_file

--- server.py
+++ server.py
@@ -1,30 +1,39 @@
 import os
+import pickle
+from settings import *
 from socket import *
 
 
-def create_file():
-    data = serverSocket.recv(1000000)
-    file = open("", 'wb')
-    file.write(data.strip())
+def receive_file(fn):
+    with open(fn, 'wb') as file:
+        while True:
+            data = server_socket.recv(BUFFER_SIZE)
+            content = pickle.loads(data)
+            print(data.decode())
+            if content == 'EOF':
+                file.close()
+                return
+            file.write(data)
 
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind(('', SERVER_PORT))
 file_prefix = "serverFiles/"
 print("The server is ready to receive.")
 while True:
-    command, clientAddress = serverSocket.recvfrom(2048)
-    commandName = command.decode().split(" ")[0]
-    match commandName:
+    command, clientAddress = server_socket.recvfrom(2048)
+    command_name = command.decode().split(" ")[0]
+    match command_name:
         case 'list':
-            serverSocket.sendto(os.listdir(file_prefix).__str__().encode(), clientAddress)
+            server_socket.sendto(os.listdir(file_prefix).__str__().encode(), clientAddress)
         case 'get':
-            fileName = command.decode().split(" ")[1]
+            file_name = command.decode().split(" ")[1]
             break
         case 'put':
-            fileName = command.decode().split(" ")[1]
-            create_file()
+            file_name = command.decode().split(" ")[1]
+            receive_file(file_prefix + file_name)
+            f = open(file_prefix + file_name, "r")
+            print(f.read())
         case 'quit':
-            serverSocket.close()
+            server_socket.close()
             break
