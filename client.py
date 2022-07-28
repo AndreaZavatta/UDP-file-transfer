65bcf86ce7396ac722d85c9aa22918f54e194e61
Luca Pasini <pasini.luca09@gmail.com>
2022-07-28 15:41:00 +0200

    Update the sending of the file name to the server

--- client.py
+++ client.py
@@ -100,35 +100,69 @@ def upload_packet_list(packet_list):
         sleep(0.1)
 
 
+# initializes the client socket and client utils
 client_socket = socket(AF_INET, SOCK_DGRAM)
 client_socket.settimeout(None)
 set_utils_socket(client_socket)
 file_prefix = os.getcwd() + "\\clientFiles\\"
+# main loop
 while True:
+    # receives the input from the user
     message = input('Input a command between list, get, put or quit to exit: ')
     command = message.split(' ')[0]
+    # checks if the command is valid
     match command:
         case 'list':
+            # gets the list of files from the server and prints it
             file_list = receive_message()
             print(file_list.decode())
         case 'get':
             client_socket.settimeout(None)
+            # sends the command to the server
             send_message((SERVER_NAME, SERVER_PORT), command)
             file_name = message.split(' ')[1]
+            # sends the file name to the server
             send_message((SERVER_NAME, SERVER_PORT), file_name)
+            # waits for the server to acknowledge the file name
             response = receive_message()
             if response.decode() == 'NACK':
                 print('File not present on server')
             elif response.decode() == 'ACK':
                 receive_file(file_prefix + file_name, receive_number_of_packets())
         case 'put':
-            client_socket.settimeout(2)
+            client_socket.settimeout(TIMEOUT)
+            # sends the command to the server
             send_message((SERVER_NAME, SERVER_PORT), command)
             file_name = message.split(' ')[1]
-            send_message((SERVER_NAME, SERVER_PORT), file_name)
-            send_file(file_prefix + file_name)
+            failed_attempts = 0
+            # sends the file name to the server and waits for the server to acknowledge the file name
+            while True:
+                send_message((SERVER_NAME, SERVER_PORT), file_name)
+                try:
+                    response = receive_message()
+                    # if the server acknowledges the file name, then the file is sent
+                    if response.decode() == file_name:
+                        send_acknowledge((SERVER_NAME, SERVER_PORT))
+                        send_file(file_prefix + file_name)
+                        break
+                    # if the server does not acknowledge the file name,
+                    # then the server notifies the client and the client tries to send the file again
+                    elif failed_attempts < MAX_FAILED_ATTEMPTS:
+                        failed_attempts += 1
+                        send_retry_acknowledge((SERVER_NAME, SERVER_PORT))
+                    # if the server does not acknowledge the file name and the client has reached the maximum number of
+                    # failed attempts then the client notifies the server and the client exits
+                    else:
+                        send_not_acknowledge((SERVER_NAME, SERVER_PORT))
+                        print('Connection failed')
+                        break
+                # if the server does not respond, then the client tries to receive the acknowledgement again
+                except error:
+                    failed_attempts += 1
+        # if the command is quit, then the client exits
         case 'quit':
             break
+        # if the command is not valid, then the client notifies the user
         case default:
             print('Invalid command')
 client_socket.close()
