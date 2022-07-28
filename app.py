f219086ccbb56a6ce8d41c547a0ac111f917cb52
Andrea Zavatta <zavattaandrea@gmail.com>
2022-07-28 03:24:54 +0200

    show list of files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
-
+import json
+from json import JSONEncoder
 from flask import request
 
 from flask import Flask, render_template
@@ -7,12 +8,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+class File:
+    def __init__(self, name, dimension):
+        self.name = name
+        self.dimension = dimension
+
+
 @app.route('/')
 def init():
     return render_template('index.html')
 
 
-@app.route('/upload-image/', methods=['GET', 'POST'])
+@app.route('/upload-file/', methods=['GET', 'POST'])
 def uploadFile():
     ret = 0
     try:
@@ -21,10 +33,18 @@ def uploadFile():
         # message_byte = byte array del file
         message_bytes = base64.b64decode(contenuto)
         nomefile = request.form.get('nomefile')
+        list.append(File(nomefile, 3))
     except:
         ret = 1
-
     return str(ret)
+
+
+list = [File("firstFile", 3), File("secondFile", 4), File("thirdFile", 27)]
+
+
+@app.route('/getFileList/', methods=['GET'])
+def getFileList():
+    return MyEncoder().encode(list)
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,4 +1,6 @@
-
+$(document).ready(function(){
+    getFileList();
+})
             function submitForm() {
               const enc = new TextDecoder("utf-8");
               const fileList = document.getElementById("filePicker").files;
@@ -10,7 +12,7 @@
 
                         $.ajax({
 
-                            'url' : 'http://127.0.0.1:5000/upload-image/',
+                            'url' : 'http://127.0.0.1:5000/upload-file/',
                             'type' : 'POST',
                             'data' : {
                                 nomefile : document.getElementById("filePicker").files[0].name,
@@ -18,9 +20,9 @@
                             },
                             'success' : function(data) {
                                 if (data == "0") {
-                                    alert("OK")
+                                    getFileList();
                                 } else {
-                                    alert("errore")
+                                    alert("errore");
                                 }
                             },
                             'error' : function(xhr, status, error)
@@ -35,7 +37,24 @@
                    }
                 }
 
-
+function getFileList(){
+        $.ajax({
+        'url' : 'http://127.0.0.1:5000/getFileList/',
+        'type' : 'GET',
+        'success' : function(list) {
+            var obj = JSON.parse(list);
+            $("#box-file").empty();
+            obj.forEach(function(el) {
+                addBoxFile(el.name);
+            });
+        },
+        'error' : function(xhr, status, error)
+        {
+            var err = eval("(" + xhr.responseText + ")");
+            alert("errore");
+        }
+    });
+}
 
 function arrayBufferToBase64( buffer ) {
 	var binary = '';
@@ -48,45 +67,15 @@ function arrayBufferToBase64( buffer ) {
 }
 
 
-            function addFile(){
+function addFile(){
+    const filePiecker = document.getElementById("filePicker");
+    const name = filePiecker.files.item(0).name;
+   $("#file-container").innerHTML+= $("#box-file").clone();
+}
 
-                const filePiecker = document.getElementById("filePicker");
-                const name = filePiecker.files.item(0).name
-               document.getElementById('row').innerHTML+=
-                   "            <div class=\"col-md-4\">\n" +
-                   "              <div class=\"card mb-4 box-shadow\">\n" +
-                   "                <div class=\"card-body\">\n" +
-                   "                  <p class=\"card-text\">"+name+".</p>\n" +
-                   "                  <div class=\"d-flex justify-content-between align-items-center\">\n" +
-                   "                    <div class=\"btn-group\">\n" +
-                   "                        <!-- Button trigger modal -->\n" +
-                   "                        <button type=\"button\" class=\"btn btn-outline-secondary\" data-toggle=\"modal\" data-target=\"#exampleModal\">\n" +
-                   "                          Info\n" +
-                   "                        </button>\n" +
-                   "            <!-- Modal -->\n" +
-                   "                    <div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n" +
-                   "                      <div class=\"modal-dialog\" role=\"document\">\n" +
-                   "                        <div class=\"modal-content\">\n" +
-                   "                          <div class=\"modal-header\">\n" +
-                   "                            <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>\n" +
-                   "                            <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n" +
-                   "                              <span aria-hidden=\"true\">&times;</span>\n" +
-                   "                            </button>\n" +
-                   "                          </div>\n" +
-                   "                          <div class=\"modal-body\">\n" +
-                   "                            info\n" +
-                   "                          </div>\n" +
-                   "                          <div class=\"modal-footer\">\n" +
-                   "                            <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n" +
-                   "                          </div>\n" +
-                   "                        </div>\n" +
-                   "                      </div>\n" +
-                   "                    </div>\n" +
-                   "                        <button type=\"button\" class=\"btn btn-outline-secondary\">Download</button>\n" +
-                   "                    </div>\n" +
-                   "                    <small class=\"text-muted\">9 mins</small>\n" +
-                   "                  </div>\n" +
-                   "                </div>\n" +
-                   "            </div>\n" +
-                   "            </div>"
-           }
+
+function addBoxFile(filename, dataupload){
+    let div=$(".hide-box").clone().removeClass("d-none hide-box");
+    div.find(".card-text").html(filename);
+    $("#box-file").append(div);
+}
